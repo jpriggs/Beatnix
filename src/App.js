6ca7ff5eb092f7e974e35f0474c3213fcb9bf886
jpriggs <jprigdon@gmail.com>
2018-11-16 22:21:27 +0900

    Added js, scss, and index files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ButtonMap from './DrumPadContainer.js';
+import './index.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+//Key mapping
+const keyDict = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Piano Chord (G major)',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+}, {
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Piano Chord (D major)',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+}, {
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Piano Chord (C major)',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+}, {
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Open-Hi-Hat',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+}, {
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Closed-Hi-Hat',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+}, {
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Top-Hi-Hat',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+}, {
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: 'Snare',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+}, {
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+}, {
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Clap',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' //Change this***
+}];
+
+//Parent Class
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      triggerDict: keyDict,
+      displayName: ''
+    }
+    this.displayName = this.displayName.bind(this);
+    this.clearName = this.clearName.bind(this);
+  }
+  displayName(name){
+    this.setState({
+      displayName: name
+    });
+  }
+  clearName(){
+    this.setState({
+      displayName: ''
+    });
+  }
+  render(){
+   return (
+     <div>
+       <div className="drum-machine" id="drum-machine">
+         <div className="logo">beatnix</div>
+         <div className="display" id="display">{this.state.displayName}</div>
+         <div className="pad-container">
+           <ButtonMap triggers={this.state.triggerDict} showName={this.displayName} />
+         </div>
+       </div>
+       <div className="footer">created by:<a href="https://github.com/jpriggs" target="_blank" rel="noopener noreferrer">jasonRigdon</a></div>
+     </div>
+   )
   }
 }
 
---
+++ src/DrumPadContainer.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './DrumPadContainer.scss';
+
+//Create individual button object **child component of ButtonMap**
+class ButtonObject extends React.Component{
+  constructor(props){
+    super(props);
+    this.playSound = this.playSound.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  componentDidMount(){
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+  componentWillUnmount(){
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+  handleKeyPress(event){
+    if (event.keyCode === this.props.keyCode){
+      document.getElementById(this.props.btnId).classList.add('drumpad-active');
+      this.playSound();
+      setTimeout(() => document.getElementById(this.props.btnId).classList.remove('drumpad-active'), 100);
+      ;
+    }
+  }
+  playSound(event){
+    const sound = document.getElementById(this.props.keyTrigger);
+    sound.currentTime = 0;
+    sound.play();
+    this.props.showName(this.props.btnId.replace(/-/g, ' '));
+  }
+  render(){
+    return (
+      <div className="drum-pad" id={this.props.btnId} onClick={this.playSound}>
+        <audio className="clip" id={this.props.keyTrigger} src={this.props.btnAudio}></audio>
+        <p>{this.props.keyTrigger}</p>
+      </div>
+    )
+  }
+}
+
+//Create all buttons in a matrix **child component of App**
+class ButtonMap extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    let buttons = this.props.triggers.map((button, idx, triggersArr) => {
+      return (
+        <ButtonObject btnId={triggersArr[idx].id} btnAudio={triggersArr[idx].url}
+          keyTrigger={triggersArr[idx].keyTrigger} keyCode={triggersArr[idx].keyCode}
+          showName={this.props.showName} />
+      )
+    });
+    return (
+      <div>{buttons}</div>
+    )
+  }
+}
+export default ButtonMap;
